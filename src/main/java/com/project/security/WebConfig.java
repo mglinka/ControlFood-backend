2bfada58812817bc3748afe21e5108f4a44dacd8
242391 Marta Glinka <242391@edu.p.lodz.pl>
2025-01-09 00:40:22 +0100

    CORS :)

--- src/main/java/com/project/security/WebConfig.java
+++ src/main/java/com/project/security/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://orange-rock-03ed9c003.5.azurestaticapps.net");
+                .allowedOrigins("http://localhost:5173", "https://orange-rock-03ed9c003.5.azurestaticapps.net")  // Zezwolenie na zapytania z tych domen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Określenie metod
+                .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")  // Dozwolone nagłówki
+                .allowCredentials(true)  // Zezwolenie na przesyłanie ciasteczek i danych uwierzytelniających
+                .maxAge(3600);  // Czas przechowywania nagłówków CORS (w sekundach)
     }
 }
 
